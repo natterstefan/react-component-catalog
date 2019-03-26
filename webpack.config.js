82aaafb0a535c1850eebbb3648b215ef4b012f6e
Stefan Natter <stefan@natter.at>
2019-03-26 08:09:50 +0100

    resolve travis issues with node 8 for now

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,6 @@ const { resolve } = require('path')
 
 module.exports = {
   entry: resolve(__dirname, 'src/index.js'),
-  externals: {
-    react: 'React',
-  },
   mode: 'development',
   module: {
     rules: [
