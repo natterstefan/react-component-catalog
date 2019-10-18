7ce9aca9b1ccdd0159a9c6881aaa6c0aae4845fe
Stefan Natter <stefan@natter.at>
2019-10-18 22:49:47 +0200

    chore: update ESLint packages, fix linting errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
     'jest/prefer-strict-equal': 'error',
     'no-underscore-dangle': 0,
     'sort-keys': 0,
+    'react/jsx-props-no-spreading': 0,
     'react/prop-types': 0,
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'error',
@@ -19,6 +20,7 @@ module.exports = {
       files: ['*.test.js'],
       rules: {
         'import/no-extraneous-dependencies': 0,
+        'max-classes-per-file': 0,
         'no-console': 0,
       },
     },
