8cfe66132b081b4671cb34150a3abd043de7d607
Stefan Natter <stefan@natter.at>
2020-09-06 08:50:48 +0200

    docs: added note about CatalogComponents usage

--- packages/example/src/client/base/components/app/index.tsx
+++ packages/example/src/client/base/components/app/index.tsx
@@ -5,11 +5,14 @@ import { CLIENTS } from '../../config'
 
 const App: FunctionComponent = () => (
   <div>
+    <CatalogComponent component="Title">Hello Base</CatalogComponent>
+    {/* use CatalogComponents _only_ when you have augmented CatalogComponents in
+     * your app! See packages/example/src/react-component-catalog.d.ts */}
     <CatalogComponent<CatalogComponents> component="Title">
-      Hello Base
+      Hello Base 2
     </CatalogComponent>
-    <CatalogComponent component="Title">Hello Base 2</CatalogComponent>
     <CatalogComponent<{ Test: ReactNode }> component="Test" />
+    <CatalogComponent<any> component="Something" />
     {CLIENTS.map(client => (
       <p key={client}>
         <a href={`/${client}`}>Open {client}</a>
