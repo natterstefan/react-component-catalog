b47cd8f3c9072eeb723c19c25a9a55bf171b9af4
Stefan Natter <stefan@natter.at>
2020-10-27 20:30:50 +0100

    chore: optimized tsconfig files and webpack configs

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,12 @@ module.exports = {
     '!src/**/*.dt.ts',
     '!src/**/(__mocks__|__stories__|__tests__)/*.{js,jsx,ts,tsx}',
   ],
+  globals: {
+    'ts-jest': {
+      diagnostics: false,
+      tsConfig: 'tsconfig.test.json',
+    },
+  },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testMatch: ['/**/__tests__/*.test.ts', '/**/__tests__/*.test.tsx'],
   testPathIgnorePatterns: ['<rootDir>/(dist|es|esm|lib|node_modules)/'],
