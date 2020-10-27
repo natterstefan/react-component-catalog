24d7441cae4041563b44172beec2c540724725f8
Stefan Natter <stefan@natter.at>
2020-10-27 19:45:12 +0100

    chore: optimized lint and travis config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,14 @@
 module.exports = {
   extends: ['eslint-config-ns-ts'],
-  plugins: ['react-hooks'],
   globals: {
     __DEV__: true,
   },
   rules: {
-    // universal rules
+    // general ESLint rules
     'no-underscore-dangle': 0,
-    'sort-keys': 0,
 
     // react settings
     'react/jsx-props-no-spreading': 0,
-    'react/prop-types': 0,
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'error',
 
     // typescript settings
     // @see https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/naming-convention.md#enforce-that-interface-names-do-not-begin-with-an-i
